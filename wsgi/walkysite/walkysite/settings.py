8c054ab4305b1a8b140bd2477b32a54ccfb674f1
yarik-tarkan <yarik_tarkan@mail.ru>
2014-10-10 15:19:28 +0400

    media folder dir added

--- wsgi/walkysite/walkysite/settings.py
+++ wsgi/walkysite/walkysite/settings.py
@@ -3,6 +3,8 @@
 import os
 import urlparse
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -70,7 +72,8 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+#MEDIA_URL = ''
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
